728abb6158415427e3455dea8d357a5bb8a226e5
Nguyen Trong Thuan <ntthuan060102.work@gmail.com>
2024-04-22 11:25:31 +0700

    update: add user profile data to api /token

--- vticket_app/services/profile_service.py
+++ vticket_app/services/profile_service.py
@@ -1,4 +1,8 @@
+from typing import Union
+
 from vticket_app.models.user import User
+from vticket_app.serializers.user_serializer import UserSerializer
+
 class ProfileService():
     def change_avatar(self, user_id: int, avatar_url: str) -> bool:
         try:
@@ -10,3 +14,11 @@ class ProfileService():
         except Exception as e:
             print(e)
             return False
+
+    def get_profile_by_email(self, email: str) -> Union[dict | None]:
+        try:
+            instance = User.objects.get(email=email)
+            return UserSerializer(instance, exclude=["password", "last_login"]).data
+        except Exception as e:
+            print(e)
+            return None
